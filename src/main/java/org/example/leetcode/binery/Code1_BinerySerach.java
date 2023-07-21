040cda1542763248351b1ff7c8752b04953f520d
liuhao <15834260040@163.com>
2023-07-21 17:35:46 +0800

    提交

---
+++ src/main/java/org/example/leetcode/binery/Code1_BinerySerach.java
@@ -0,0 +1,8 @@
+package org.example.leetcode.binery;/**
+*@program: Leetcode
+*@description: 
+*@author: liuhao
+*
+*@date: 2023-07-18 09:38
+*/public class Code1_BinerySerach {
+}
---
+++ src/main/java/org/example/leetcode/binery/Code2_BineryAreaMin.java
@@ -0,0 +1,10 @@
+package org.example.leetcode.binery;
+
+/**
+ * @program: Leetcode
+ * @description: 二分法查询某个数是否存在
+ * @author: liuhao
+ * @date: 2023-07-18 09:38
+ */
+public class Code1_BinerySerach {
+}
