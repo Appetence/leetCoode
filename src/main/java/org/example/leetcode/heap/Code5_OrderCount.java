abd275d0a72fc0c038e5bdbead089699b5e42115
liuhao <15834260040@163.com>
2024-01-11 20:49:25 +0800

    购买订单统计

--- src/main/java/org/example/leetcode/heap/Code5_OrderCount.java
+++ src/main/java/org/example/leetcode/heap/Code5_OrderCount.java
@@ -1,10 +1,15 @@
 package org.example.leetcode.heap;
 
+import java.text.Collator;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
 
 /**
  * 不同时间点，客户购买了商品,获取购买数最多的用户
@@ -12,18 +17,15 @@ import java.util.Optional;
  * 统计购买数量最多的用户
  */
 public class Code5_OrderCount {
-  private HeapCreaterQuere<Entry<Integer>> giftArea = new HeapCreaterQuere<Entry<Integer>>(
-      new WaitOrderComparator(), 3);
-  // 候选区
-  private HeapCreaterQuere<Entry<Integer>> waitArea = new HeapCreaterQuere<Entry<Integer>>(
-      new WaitOrderComparator(), 5);
+  // 得奖区
 
   public static void main(String[] args) {
     // 用户
     int[] people = { 1, 2, 3, 4, 5, 6, 2, 3, 2, 1, 1 };
     // 行为
-    int[] action = { 1, 2, -1, 4, 5, 6, 2, -3, 2, 1, 1 };
-
+    int[] action = { 1, 0, 1, 1, 1, 1, 1, 0, 1, 0, 1 };
+    // 奖池大小
+    int prizeLimit = 0;
     // 获奖区
 
     /**
@@ -37,143 +39,22 @@ public class Code5_OrderCount {
      * 小于
      * 
      */
-    buyGift(people, action);
+    takeK(people, action, prizeLimit);
 
   }
 
-  private static void buyGift(int[] people, int[] action) {
-    for (int idx = 0; idx < people.length; idx++) {
+  private static List<List<Integer>> takeK(int[] people, int[] action, int prizeLimit) {
+    WhoYourDaddy whoDaddies = new WhoYourDaddy(3);
+    List<List<Integer>> ans = new ArrayList<>();
 
+    for (int i = 0; i < people.length; i++) {
+      // 行为处理
+      whoDaddies.operate(i, people[i], action[i]);
+      // 每次操作后的结构都记录一下
+      ans.add(whoDaddies.getDaddies());
     }
-  }
+    return ans;
 
-  /**
-   * 自定义 堆，实现 O(N) 获取指定节点
-   */
-  class HeapCreaterQuere<T> {
-
-    // index
-    private Map<Entry<Integer>, Integer> map = new HashMap<Entry<Integer>, Integer>();
-
-    private int heapSize;
-
-    private Comparator<Entry<Integer>> comparator;
-    // 堆
-    private ArrayList<Entry<Integer>> heap;
-
-    public HeapCreaterQuere(Comparator<Entry<Integer>> comparator, int heapSize) {
-      this.heap = new ArrayList<>();
-      this.heapSize = heapSize;
-      this.comparator = comparator;
-    }
-
-    public boolean isEmpty() {
-      return heapSize == 0;
-    }
-
-    public String each() {
-      String result = "";
-      for (Entry<Integer> heap : heap) {
-        result = result + heap.getData().intValue() + " ";
-      }
-      return result;
-    }
-
-    public Integer push(Integer data, Integer time) {
-      Entry<Integer> entry = new Entry<Integer>(data, time);
-      if (map.containsKey(entry)) {
-        // 已存在的对象
-        return map.get(entry);
-        // TODO
-      } else {
-        heap.add(entry);
-        map.put(entry, heapSize);
-        int size = heapSize++;
-        heapInsert(size);// 大根堆
-        // 保证每次新增都能走到heapIfy
-
-        return data;
-      }
-    }
-
-    public void heapInsert(int index) {
-
-      int parebtIndex = (index - 1) / 2;
-
-      while (comparator.compare(heap.get(parebtIndex), heap.get(index)) < 0) {
-        swap(parebtIndex, index);
-        index = parebtIndex;
-        parebtIndex = (index - 1) / 2;
-      }
-
-    }
-
-    public void swap(int oleIdx, int newIdx) {
-      Entry<Integer> entryOld = heap.get(oleIdx);
-      Entry<Integer> entryNew = heap.get(newIdx);
-      heap.set(oleIdx, entryNew);
-      heap.set(newIdx, entryOld);
-      map.put(entryNew, oleIdx);
-      map.put(entryOld, newIdx);
-    }
-
-    // 大根堆
-    public void heapify(int index) {
-      int left = 2 * index + 1;
-      while (heapSize > left) {
-        int max = ((heapSize > left + 1) && (comparator.compare(heap.get(left), heap.get(left + 1)) < 0)) ? left + 1
-            : left;
-        // 判读最大的节点是哪个
-        max = (comparator.compare(heap.get(max), heap.get(index)) < 0) ? index : max;
-        if (max == index) {
-          break;
-        }
-        swap(max, index);
-        index = max;
-        left = 2 * left + 1;
-      }
-    }
-
-    private boolean resign(Integer t1, Integer t2) {
-
-      return false;
-    }
-
-    public Entry<Integer> peek() {
-      return heap.get(0);
-    }
-
-    /**
-     * 取出头结点
-     * 
-     * @return
-     */
-    public Entry<Integer> pop() {
-      Entry<Integer> entry = heap.get(0);
-      swap(0, heapSize - 1);
-      map.remove(entry);
-      heap.remove(--heapSize);
-      heapify(0);
-      return entry;
-    }
-
-    public Entry<Integer> remove(Integer data) {
-
-      Optional<Entry<Integer>> option = map.entrySet().stream().filter(item -> item.getKey().getData().equals(data))
-          .map(item -> item.getKey()).findFirst();
-      if (option.isPresent()) {
-        Entry entry = option.get();
-        map.remove(entry);
-        heap.remove(entry);
-        return entry;
-      } else {
-        return null;
-      }
-    }
-
-    public int size() {
-      return heapSize;
-    }
   }
 
 }
@@ -211,7 +92,8 @@ class GiftOrderComparator implements Comparator<Entry<Integer>> {
 
   @Override
   public int compare(Entry<Integer> o1, Entry<Integer> o2) {
-    return o1.getData().equals(o2.getData()) ? o1.getTime() - o2.getTime() : o1.getData() - o2.getData();
+    // 购买单数少的在前边 单数相同时候先进来的在前边
+    return o1.getData().equals(o2.getData()) ? o2.getTime() - o1.getTime() : -(o1.getData() - o2.getData());
   }
 
 }
@@ -223,7 +105,199 @@ class WaitOrderComparator implements Comparator<Entry<Integer>> {
 
   @Override
   public int compare(Entry<Integer> o1, Entry<Integer> o2) {
-    return o1.getData().equals(o2.getData()) ? o2.getTime() - o1.getTime() : o2.getData() - o1.getData();
+    // 购买单数多的在前边 单数相同时候先进来的在前边
+    return o1.getData().equals(o2.getData()) ? o1.getTime() - o2.getTime() : o1.getData() - o2.getData();
   }
 
+}
+
+class Customer {
+  private int number;
+  private int time;
+
+  public int getNumber() {
+    return number;
+  }
+
+  public void setNumber(int number) {
+    this.number = number;
+  }
+
+  public int getTime() {
+    return time;
+  }
+
+  public void setTime(int time) {
+    this.time = time;
+  }
+
+}
+
+class WhoYourDaddy {
+  private HeapCreaterQuere<Customer> daddyHeap;
+  // 候选区
+  private HeapCreaterQuere<Customer> condHeap;
+  // id 对应custom
+  private Map<Integer, Customer> map;
+  // 范围大小
+  private final int limit;
+
+  public WhoYourDaddy(int limit) {
+    this.daddyHeap = new HeapCreaterQuere<Customer>(
+        new GiftOrderComparator(), 3);
+    this.condHeap = new HeapCreaterQuere<Customer>(
+        new WaitOrderComparator(), 5);
+    this.map = new HashMap<>();
+    this.limit = limit;
+  }
+
+  public List<Integer> getDaddies() {
+    daddyHeap.getAllElements()
+    return null;
+  }
+/**
+ * 当前处理i号事件
+ * @param i
+ * @param j
+ * @param k
+ */
+  public void operate(int i, int j, int k) {
+
+  }
+
+
+
+}
+
+/**
+ * 自定义 堆，实现 O(N) 获取指定节点
+ */
+class HeapCreaterQuere<T> {
+
+  // index
+  private Map<Entry<Integer>, Integer> map = new HashMap<Entry<Integer>, Integer>();
+
+  private int heapSize;
+
+  private Comparator<Entry<Integer>> comparator;
+  // 堆
+  private ArrayList<Entry<Integer>> heap;
+
+  public HeapCreaterQuere(Comparator<Entry<Integer>> comparator, int heapSize) {
+    this.heap = new ArrayList<>();
+    this.heapSize = heapSize;
+    this.comparator = comparator;
+  }
+
+  public boolean isEmpty() {
+    return heapSize == 0;
+  }
+
+  public String each() {
+    String result = "";
+    for (Entry<Integer> heap : heap) {
+      result = result + heap.getData().intValue() + " ";
+    }
+    return result;
+  }
+
+  public List<T> getAllElements{
+    return heap.stream().collect(Collectors.toList());
+
+  }
+  public Integer push(Integer data, Integer time) {
+    Entry<Integer> entry = new Entry<Integer>(data, time);
+    if (map.containsKey(entry)) {
+      // 已存在的对象
+      return map.get(entry);
+      // TODO
+    } else {
+      heap.add(entry);
+      map.put(entry, heapSize);
+      int size = heapSize++;
+      heapInsert(size);// 大根堆
+      // 保证每次新增都能走到heapIfy
+
+      return data;
+    }
+  }
+
+  public void heapInsert(int index) {
+
+    int parebtIndex = (index - 1) / 2;
+
+    while (comparator.compare(heap.get(parebtIndex), heap.get(index)) < 0) {
+      swap(parebtIndex, index);
+      index = parebtIndex;
+      parebtIndex = (index - 1) / 2;
+    }
+
+  }
+
+  public void swap(int oleIdx, int newIdx) {
+    Entry<Integer> entryOld = heap.get(oleIdx);
+    Entry<Integer> entryNew = heap.get(newIdx);
+    heap.set(oleIdx, entryNew);
+    heap.set(newIdx, entryOld);
+    map.put(entryNew, oleIdx);
+    map.put(entryOld, newIdx);
+  }
+
+  // 大根堆
+  public void heapify(int index) {
+    int left = 2 * index + 1;
+    while (heapSize > left) {
+      int max = ((heapSize > left + 1) && (comparator.compare(heap.get(left), heap.get(left + 1)) < 0)) ? left + 1
+          : left;
+      // 判读最大的节点是哪个
+      max = (comparator.compare(heap.get(max), heap.get(index)) < 0) ? index : max;
+      if (max == index) {
+        break;
+      }
+      swap(max, index);
+      index = max;
+      left = 2 * left + 1;
+    }
+  }
+
+  private boolean resign(Integer t1, Integer t2) {
+
+    return false;
+  }
+
+  public Entry<Integer> peek() {
+    return heap.get(0);
+  }
+
+  /**
+   * 取出头结点
+   * 
+   * @return
+   */
+  public Entry<Integer> pop() {
+    Entry<Integer> entry = heap.get(0);
+    swap(0, heapSize - 1);
+    map.remove(entry);
+    heap.remove(--heapSize);
+    heapify(0);
+    return entry;
+  }
+
+  public Entry<Integer> remove(Integer data) {
+
+    Optional<Entry<Integer>> option = map.entrySet().stream().filter(item -> item.getKey().getData().equals(data))
+        .map(item -> item.getKey()).findFirst();
+    if (option.isPresent()) {
+      Entry entry = option.get();
+      map.remove(entry);
+      heap.remove(entry);
+      return entry;
+    } else {
+      return null;
+    }
+  }
+
+  public int size() {
+    return heapSize;
+  }
 }
