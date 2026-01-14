f1877fe522ba25ae2ccd4e57b234479a74b47538
liuhao <liuhao@wejoy.com>
2026-01-14 14:24:45 +0800

    feat: 无效配置删除，vim常用技巧

--- src/main/java/org/example/test/mainTest.java
+++
@@ -1,8 +0,0 @@
-package org.example.test;
-
-public class mainTest {
-  public static void main(String[] args) {
-    String str = "abbdsa";
-    System.out.println(str.indexOf("a"));
-  }
-}
---
+++ src/main/java/org/example/vimUtil/mainTest.java
@@ -0,0 +1,22 @@
+package org.example.vimUtil;
+
+public class mainTest {
+  public static void main(String[] args) {
+    String str = "abbdsa";
+    System.out.println(str.indexOf("a"));
+    //
+    
+    // test + test + test+test+test+test
+    // f+ s( cw )
+    // test + test + test + test + test + test
+    // test + test + test+test+test+test
+    // f +  ; 下一个 , 上一个
+    // test + test + test+test+test+test
+    // * cw n .
+    // test + next + next + next + next + next
+    // 
+    // test + test + test+test+test+test
+
+    // 3
+  }
+}
